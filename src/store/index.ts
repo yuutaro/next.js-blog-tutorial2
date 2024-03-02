ec022c8da46cd4ade7232df2cc8ad671ed6b8e25
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-03 02:44:07 +0900

    redux-persistを用いてstoreを永続化
    https://github.com/rt2zz/redux-persist?tab=readme-ov-file#state-reconciler
    https://zenn.dev/nus3/articles/c2d86097029c12285680

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import themeReducer from "./modules/themeSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default configureStore({
-  reducer: {
-    theme: themeReducer,
-  }
+const rootReducer = combineReducers({
+  theme: themeReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
