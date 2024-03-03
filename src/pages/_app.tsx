b983e6d7a0185ff80305aca3a436f20b96b5cb6f
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-03 15:12:26 +0900

    node.js上にstoreを永続化するためのWebストレージが作成できずエラーを吐く問題を修正
    https://zenn.dev/nus3/articles/c2d86097029c12285680

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-// storeを永続化するために必要なオブジェクト。
-import { persistor, store } from "@/store";
-
-
+import { useStore } from "@/store";
+import { persistStore } from "redux-persist";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const store = useStore();
+  const persistor = persistStore(store);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,27 +1,44 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, EnhancedStore } from "@reduxjs/toolkit";
 import themeReducer from "./modules/themeSlice";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, persistReducer, persistStore } from "redux-persist";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
-
-// まず、combineReducers関数を使用して、複数のreducerを一つにまとめています。この例では、themeReducerという名前のreducerがあり、それがthemeというキーに関連付けられています。これにより、state.themeを通じてテーマに関する状態にアクセスできます。
 const rootReducer = combineReducers({
   theme: themeReducer,
 });
 
-// 次に、Redux Persistの設定を定義しています。persistConfigオブジェクトでは、永続化のキーとストレージエンジンを指定しています。キーは"root"となっており、これは永続化されるデータのルートを表します。storageは、データを保存するためのストレージエンジンを指定します。
+// HACK: `redux-persist failed to create sync storage. falling back to noop storage.`の対応
+// https://github.com/vercel/next.js/discussions/15687#discussioncomment-45319
+const createNoopStorage = () => {
+  return {
+    getItem(_key: any) {
+      return Promise.resolve(null)
+    },
+    setItem(_key: any, value: any) {
+      return Promise.resolve(value)
+    },
+    removeItem(_key: any) {
+      return Promise.resolve()
+    },
+  }
+}
+const storage = typeof window === 'undefined' ? createNoopStorage() : createWebStorage('local');
+
 const persistConfig = {
   key: "root",
   storage,
 };
 
-// その後、persistReducer関数を使用して、永続化を可能にするreducerを作成しています。この関数は、指定した設定とreducerを引数に取り、新しいreducerを返します。この新しいreducerは、アプリケーションの状態を永続化するために使用されます。
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// そして、configureStore関数を使用して、Reduxストアを作成しています。この関数は、アプリケーションの状態を管理するための中心的な場所を提供します。ここでは、永続化されたreducerを使用してストアを設定しています。
-export const store = configureStore({
-  reducer: persistedReducer,
-});
-
-// _app.tsxの<PersistGate loading={null} persistor={persistor}>で使用
-export const persistor = persistStore(store);
+export const useStore = (): EnhancedStore => {
+  return configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        }
+      })
+  });
+};
