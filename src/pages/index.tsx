0111409708387fc96d1f9288c7d8c25a9f8f5018
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-03 12:13:28 +0900

    テーマ変更プルダウン作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,12 +22,16 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>My Next.js Site</title>
-      </Head>
       <Theme>
         <h1>Hello world!</h1>
-        <input type="button" onClick={handler} value={theme} />
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn m-1">Click</div>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            {themes.map((t) => (
+              <li key={t} onClick={() => dispatch(changeTheme(t))}><a>{t}</a></li>
+            ))}
+          </ul>
+        </div>
       </Theme>
     </>
   );
