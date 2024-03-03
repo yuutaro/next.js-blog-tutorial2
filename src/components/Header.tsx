85245ba371155955051e8d1effac02d9686823fb
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-03 22:39:13 +0900

    ./index.html仮作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,13 +12,13 @@ export default function Header({ children }: HeaderProps): React.ReactElement {
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col">
           {/* Navbar */}
-          <div className="w-full navbar bg-base-300">
+          <div className="w-full navbar bg-base-300 fixed z-40">
             <div className="flex-none">
               <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
               </label>
             </div>
-            <div className="flex-1 px-2 mx-2">Navbar Title</div>
+            <div className="flex-1 px-2 mx-2">Waxworkのブログ</div>
             <div className="flex-none hidden lg:block">
               <ul className="menu menu-horizontal">
                 {/* Navbar menu content here */}
@@ -30,7 +30,7 @@ export default function Header({ children }: HeaderProps): React.ReactElement {
           {/* Page content here */}
           {children}
         </div>
-        <div className="drawer-side">
+        <div className="drawer-side z-50">
           <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 min-h-full bg-base-200">
             {/* Sidebar content here */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,13 @@ import Theme from "@/components/theme";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import { client } from "@/libs/client";
+import React from "react";
+import Footer from "@/components/Footer";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home(props: any) {
+export default function Home(props: any): React.ReactElement {
   const { contents } = props;
   console.log(contents);
   return (
@@ -17,15 +20,18 @@ export default function Home(props: any) {
       </Head>
       <Theme>
         <Header>
-          <div>hello world!</div>
-          {contents.map((content: any) => {
-            return (
-              <div key={content.id}>
-                <h2>{content.title}</h2>
+          <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">Waxwork_ymのブログ</h1>
+                <p className="mb-5">to be, or not to be.</p>
+                <button className="btn btn-primary">Get Started</button>
               </div>
-            );
-          })}
+            </div>
+          </div>
         </Header>
+        <Footer />
       </Theme>
     </>
   );
