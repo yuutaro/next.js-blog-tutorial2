24cdb012ce7ef5895350137803f8893389cb9713
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-03 16:27:26 +0900

    MicroCMSから記事を取得、仮表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,42 @@ import Header from "@/components/Header";
 import Theme from "@/components/theme";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { client } from "@/libs/client";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-
+export default function Home(props: any) {
+  const { contents } = props;
+  console.log(contents);
   return (
     <>
       <Head>
         <title>Home | My WeBlog</title>
+        <meta name="description" content="My personal blog HomePage" />
       </Head>
       <Theme>
         <Header>
           <div>hello world!</div>
+          {contents.map((content: any) => {
+            return (
+              <div key={content.id}>
+                <h2>{content.title}</h2>
+              </div>
+            );
+          })}
         </Header>
       </Theme>
     </>
   );
+}
+
+export const getStaticProps = async () => {
+  const data: Blogs = await client.get({
+    endpoint: "blogs",
+  });
+  return {
+    props: {
+      contents: data.contents,
+    },
+  }
 }
---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+type Blog = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+  eyecatch: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  category: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    name: string;
+  };
+};
+
+type Blogs = {
+  contents: Blog[];
+  totalCount: number;
+  offset: number;
+  limit: number;
+};
